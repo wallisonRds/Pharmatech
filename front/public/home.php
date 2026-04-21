bfc39dcc919ac581fcdfa4b81f7bdd8a1e302fa1
WALLISON VIEIRA SANTOS PEREIRA <wallisonn13@gmail.com>
2026-04-21 20:33:42 -0300

    feat: implementa header com busca e animacoes de alarme

--- front/public/home.php
+++ front/public/home.php
@@ -8,6 +8,9 @@
 <body>
     <div class="layout-container">
         <?php include_once __DIR__ . "/../src/components/sidebar.inc.php"; ?>
+          <div class="main-wrapper">
+            <?php include_once __DIR__ . "/../src/components/header.inc.php"; ?>
+        </div>
     </div>
     <script type="module" src="/js/main.js"></script>
 </body>
---
+++ front/src/components/header.inc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmatech</title>
+</head>
+<body>       
+    <header>
+    <img src="assets/icons/sidebar.svg" alt="siderbar icon">
+    <div class="input-header"> 
+        <input type="text" placeholder="Buscar Produtos, Fornecedores...">
+    </div>
+    <div class="icons-right">
+        <img src="assets/icons/Alarm.svg" alt="icone de alarme">
+        <img src="assets/icons/dark-mode.svg" alt="icone de dark mode">
+        <img src="assets/icons/Profile.svg" alt="icone de perfil">
+    </div>
+    </header>
+</body>
+</html>
